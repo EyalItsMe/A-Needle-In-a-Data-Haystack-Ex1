8e96f1e6e142ef3425ae7605828acf502c83a4ef
Anton Kravtsov <anton.kravtsov@mail.huji.ac.il>
2024-06-12 11:32:50 +0300

    some minor updates in the festival.py

--- festival.py
+++ festival.py
@@ -6,7 +6,8 @@ import pandas as pd
 def tfidf(file_path, terms, festival=True):
     names = []
 
-    tf_idf_res = {term: [] for term in terms}  # will hold the final results - tf_idf and temporarily the tf results
+    # will hold the final results - tf_idf and temporarily the tf results
+    tf_idf_res = {term: [] for term in terms}
 
     num_of_docs_in_corpus = 0
     terms_total_rows_count_dict = {term: 0 for term in terms}
@@ -27,10 +28,9 @@ def tfidf(file_path, terms, festival=True):
                 terms_total_rows_count_dict[term] += 1  # counting number of rows each term appears in
                 tf_res_per_row_tmp[term] += 1  # counting number of appearances of each term in each row
 
-        # counting tf for each row (for each festival)
-        num_of_terms_in_doc = len(desc)
+        # counting tf for each row
         for term in tf_res_per_row_tmp:
-            tf_idf_res[term].append(tf_res_per_row_tmp[term] / num_of_terms_in_doc)
+            tf_idf_res[term].append(tf_res_per_row_tmp[term])
 
         names.append(row[0])  # saving name of the row
         num_of_docs_in_corpus += 1  # counting number of rows
@@ -45,37 +45,13 @@ def tfidf(file_path, terms, festival=True):
     return festivals_tf_idf
 
 
-if __name__ == '__main__':
-    path = r'C:\Users\Admin\Desktop\Uni\Courses\haystack\music_festivals.csv'
+def question_7_a_1():
+    path = r'music_festivals.csv'
     terms = ['annual', 'music', 'festival', 'soul', 'jazz', 'belgium', 'hungary', 'israel', 'rock', 'dance', 'desert',
              'electronic', 'arts']
     print(tfidf(path, terms).to_string())
 
-# a (2)
-# soul:
-# num of docs contains term - 1
-# num of docs in corpus - 11
-# idf = log (11 / 1) = 1.041
-# tf_per_festival = [0/10, 0/8, 0/8, 0/9, 0/13, 0/10, 0/10, 0/8, 0/11, 1/12, 0/12]
-# tf_idf_res_soul = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0.0867, 0]
 
-
-# festival
-# num of docs contains term - 11
-# num of docs in corpus - 11
-# idf = log (11 / 11) = 0
-# tf_per_festival = [1/10, 1/8, 1/8, 1/9, 1/13, 1/10, 1/10, 1/8, 1/11, 1/12, 1/12]
-# tf_idf_res_soul = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-# the tf-idf of soul and festival expected to be very different because of the way the idf is calculated. the fact that
-# festival appears in every row, zeroes the idf in contrast that soul appears in only 1 row which maximizes the idf
-
-
-# todo need to write it clearer (for each row)
-# (b)
-# first of all we will normalize the number of participants by dividing the given number by the number of the world
-# population at that year or by the population in each country (if it's a local festival without foreign visitors)
-# maybe we can divide the population by the size of the country in square meters
-# 2nd we will normalize the price to the worth of it be today, for example 700 USD in 1970 would be 5656 USD today
-
+if __name__ == '__main__':
+    question_7_a_1()
 
